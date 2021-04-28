2d1d48fd84f8d9484e904ef90ddfd666a1b72f55
kly3827 <82614848+kly3827@users.noreply.github.com>
2021-04-27 17:51:00 -0700

    Update Welcome03_List.java

--- Welcome03_List.java
+++ Welcome03_List.java
@@ -14,6 +14,18 @@ public class Welcome03_List {
              "station/latitude", "station/longitude");
       System.out.println("Total stations: " + allstns.size());
       
+      System.out.println();
+      WeatherStation temp = new WeatherStation("","","", 0.0, 0.0);
+      temp = allstns.get(0);
+      for(int i = 1; i<allstns.size(); i++)
+      {
+         if(allstns.get(i).getLat()<temp.getLat()){
+            temp = allstns.get(i);
+         }
+      }
+      System.out.println(temp.getId() + " is the farthest south.");
+      System.out.println();
+      
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
       String state = sc.next();
